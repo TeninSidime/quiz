5be5983314e1d71b7dc775f60af9f9a37bc8025e
Tenin Sidime <sidime.tenin@gmail.com>
2019-01-02 22:44:30 +0000

    function

--- js/script.js
+++ js/script.js
@@ -9,6 +9,25 @@ $(document).ready(function() {
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
         var totalScore;
+        rainbow(q1Result, q2Result, q3Result);
     });
 
 });
+
+function rainbow  (answer1, answer2, answer3){
+    var red= 0;
+    var blue = 0;
+    var yellow =0;
+    var error = false;
+    if ( answer1 === "yes" ) {
+        red += 1;
+    } else if ( answer2 === "maybe"){
+        blue += 1;
+        
+    }else if (answer3 ==="no"){
+        yellow += 1;
+    } else {
+        error = true;
+    }
+     
+} 
