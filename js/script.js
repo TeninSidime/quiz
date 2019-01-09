d3fc7e269e8c061c71439abf7235ecb3bccf59a3
Tenin Sidime <sidime.tenin@gmail.com>
2019-01-09 22:45:02 +0000

    scoreThing

--- js/script.js
+++ js/script.js
@@ -8,26 +8,52 @@ $(document).ready(function() {
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
-        var totalScore;
-        rainbow(q1Result, q2Result, q3Result);
+        var totalScore ;
     });
 
 });
 
 function rainbow  (answer1, answer2, answer3){
-    var red= 0;
-    var blue = 0;
-    var yellow =0;
-    var error = false;
-    if ( answer1 === "yes" ) {
-        red += 1;
-    } else if ( answer2 === "maybe"){
-        blue += 1;
-        
-    }else if (answer3 ==="no"){
-        yellow += 1;
-    } else {
-        error = true;
-    }
-     
-} 
+    //var red= 0;
+    //var blue = 0;
+    //var yellow =0;
+    //var error = false;
+      var score= 0;
+    
+   if (answer1 === "Yes" ){
+       red += 20;
+   } else if (answer1 === "Maybe"){
+       yellow += 10;
+   } else if (answer1 === "No"){
+       blue += 5 
+   } 
+   
+    if (answer2 === "Yes" ){
+       red += 20;
+   } else if (answer2 ===  "Maybe"){
+       yellow += 10;
+   } else if (answer2 === "No"){
+       blue += 5 
+   } 
+   
+    if (answer3 === "Yes" ){
+       red += 20;
+   } else if (answer3 ===  "Maybe"){
+       yellow += 10;
+   } else if (answer3 === "No"){
+       blue =+ 5 
+   } 
+}
+
+function questions(score){
+     if ( score ===60 ){
+        return "Red" ;
+    } else if (score  === 30 && score < 29 ){
+        return "Yellow";
+    } else if ( score === 15 && score < 14)
+        return "Blue";
+    } else { return "You'r not part of the rainbow.... SORRY";}
+    
+}
+
+totalScore = question1 += question2 += question3
